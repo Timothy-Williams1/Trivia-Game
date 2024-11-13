fb32771cad6273cd5a6d76ca69501d5ac735474c
Timothy Williams <Timothy.Williams@stu.glasgow.kyschools.us>
2024-11-13 08:32:29 -0600

    Initial Commit

---
+++ Trivia Game/Trivia_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trivia_GameApp.swift
+//  Trivia Game
+//
+//  Created by Student on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Trivia_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
